b3fe87a7bdb78abdf13a56eee84ddd0966a71c7b
Rick <japa.eihara@gmail.com>
2016-06-15 23:11:20 -0300

    Compiler.java pronto e criando arquivos C

--- Trabalho 5/Compiladores/src/Main.java
+++ Trabalho 5/Compiladores/src/Main.java
@@ -5,7 +5,9 @@
  */
 import AST.Program;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -19,50 +21,62 @@ import java.util.Scanner;
  */
 public class Main {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) throws IOException {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
 
-		String nome = args[0];
-		String nomeC = args[1];
+        String nome = args[0];
+        String nomeC = args[1];
 
-		String entrada = new String();
-		String linha = null;
-		try {
-			FileReader arq = new FileReader(nome);
-			BufferedReader lerArq = new BufferedReader(arq);
+        String entrada = new String();
+        String linha = null;
+        try {
+            FileReader arq = new FileReader(nome);
+            BufferedReader lerArq = new BufferedReader(arq);
 
-			linha = lerArq.readLine();
-			while (linha != null) {
-				entrada = entrada.concat(linha).concat("\n");
-				linha = lerArq.readLine();
+            linha = lerArq.readLine();
+            while (linha != null) {
+                entrada = entrada.concat(linha).concat("\n");
+                linha = lerArq.readLine();
 
-			}
-			arq.close();
-		} catch (IOException e) {
-			System.err.printf("Erro na abertura do arquivo: %s.\n",
-				e.getMessage());
-		}
+            }
+            arq.close();
+        } catch (IOException e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n",
+                    e.getMessage());
+        }
+        
 
-		entrada = entrada.concat(" ");
+        entrada = entrada.concat(" ");
 
-		char[] input = entrada.toCharArray();
-		Compiler compiler = new Compiler();
+        char[] input = entrada.toCharArray();
+        Compiler compiler = new Compiler();
 
-		ArrayList<Program> program = null;
-		program = compiler.compile(input, nome);
+        ArrayList<Program> program = null;
+        program = compiler.compile(input, nome);
 
-		if (program != null) {
-			try {
-				for (Program s : program) {
-//					s.genC(nomeC);
-				}
-			} catch (RuntimeException e) {
-				System.out.println("Não foi possível gerar C, erro de " + e);
-			}
-		}
+        File file = new File(nomeC);
 
-	}
+        FileWriter writer = new FileWriter(file);
+        StringBuffer aux;
+        Integer i = 0;
+
+        if (program != null) {
+            writer.append("#include <stdio.h>\n");
+            writer.append("#include <stdlib.h>\n\n");
+            for (Program s : program) {
+                if (i != 0) {
+                    writer.append("\n");
+                }
+                aux = s.genC(0);
+                writer.append(aux);
+                i++;
+            }
+
+            writer.close();
+        }
+
+    }
 
 }
